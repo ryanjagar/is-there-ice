3779a87b7ee5a368cd4bdbb54ee0f460b0cd7538
ryanjagar <49032969+ryanjagar@users.noreply.github.com>
2022-02-20 22:54:59 -0700

    Update deno to resolve import error

--- postprocess.js
+++ postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
+import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.14/mod.ts' 
 
 const daysToKeep = -10
 // Step 1: Read the downloaded_filename JSON
